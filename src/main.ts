b653f5bcf281db5dfb5c7e626e3aa59ceb99992c
shantanu kulkarni <shantanu@nulligma.com>
2022-05-07 16:53:09 +0530

    initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,32 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import HelloWorldScene from "./scenes/HelloWorldScene";
 
-const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
-	},
-	scene: [HelloWorldScene]
+class Test extends Phaser.Scene {
+  constructor() {
+    super("test");
+  }
+
+  preload() {
+    this.load.image("house", "assets/rem_0002.png");
+  }
+
+  create() {
+    this.add.sprite(100, 100, "house");
+    this.scene.start("hello-world");
+  }
 }
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: [Test, HelloWorldScene],
+};
 
-export default new Phaser.Game(config)
+export default new Phaser.Game(config);
